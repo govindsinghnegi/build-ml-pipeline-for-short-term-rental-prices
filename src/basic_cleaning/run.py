5a20faf227f0fb729be443f17c48e7767109bb0d
gsn <gsn@example.com>
2022-07-25 23:05:31 -0700

    filter based on geo location to fix the unit test failure

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,10 @@ def go(args):
     logger.info("Converting last_review to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # fix the unit test failure
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
 
